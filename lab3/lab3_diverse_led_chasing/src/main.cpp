ceb0791fcd6f40f9dbeac7f0b955a2d7d706a029
Simone Magri <simone.j.magri@gmail.com>
2026-03-03 14:48:19 +1100

    lab3_diverse_led_chasing, pattern3() and pattern4() implemented.

---
+++ lab3/lab3_diverse_led_chasing/src/main.cpp
@@ -0,0 +1,61 @@
+#define F_CPU 16000000UL
+
+//#include <Arduino.h>
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+void pattern1(){
+    
+}
+
+void pattern2(){
+
+    
+}
+
+void pattern3(){
+    // All LEDs on then all LEDs off.
+    
+    PORTB = 0x3F;
+    _delay_ms(500);
+    PORTB = 0x00;
+    _delay_ms(500);
+}
+
+void pattern4(){
+    // MSB then LSB of PORTB On
+
+    
+    PORTB = 0x20;
+    _delay_ms(1000);
+    PORTB = 0x01;
+    _delay_ms(1000);
+    
+}
+
+
+
+
+int main( )
+{
+
+    // Implements diverse  8 bit LED chaser using ports on the arduino uno rev3.
+    DDRB = 0x3F;
+    DDRC = 0x03;
+
+    // So the input pins are not set to high due to the arduino input pins
+    // being tied high by default.
+    PORTB = 0x00;
+    PORTC = 0x00;
+
+
+    while (1) {
+
+        pattern3();
+        pattern4();
+
+    }
+
+
+}
