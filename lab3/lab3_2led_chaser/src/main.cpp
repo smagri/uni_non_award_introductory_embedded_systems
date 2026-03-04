69730a0dffa51d3172f7b3918b15bb6459316828
Simone Magri <simone.j.magri@gmail.com>
2026-03-04 14:17:24 +1100

    LEDs move from right to left the left to right as required.

--- lab3/lab3_2led_chaser/src/main.cpp
+++ lab3/lab3_2led_chaser/src/main.cpp
@@ -3,10 +3,13 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+// Otherwise the CPU moves to fast  and we don't see any LEDs turn
+// on or off.
+#define delay_time_ms 1000
 
 // Implements an 8 bit LED chaser using ports on the arduino uno rev3.
 
-void physical_ports_to_virtual_port(char vport){
+void virtual_port_to_physical_port(char vport){
 
     // Turn current led off before next led is put on.
     PORTB = 0x00; 
@@ -33,69 +36,57 @@ int main(void)
 
     unsigned char vport = 0x00;
 
-    unsigned char led_left_to_right_on = 0;
-    unsigned char led_right_to_left_on = 0;
-
-    // Otherwise the CPU moves to fast  and we don't see any LEDs turn
-    // on or off.
-    uint16_t delay_time_ms = 500;
-
     // True when moving forwards and false for moving backwards.
-    bool turn_on_leds_from_left2right = 1;
+    bool turn_on_leds_from_left2right = 0;
     bool hold_next = 0;
 
+    // int8_t to let led_on_index go -ve for loops to work.
+    int8_t led_on_index = 6;
     
     while (1) {
 
-        if (hold_next){
-            hold_next = 0;
-        }
-        else if (turn_on_leds_from_left2right){
+        if (turn_on_leds_from_left2right){
             // Turn on LEDs left2right, or forwards.
             Serial.print("dbg: left to right led_left_to_right_on=");
-            Serial.println(led_left_to_right_on);
+            Serial.println(led_on_index);
             // Turn on portB LEDs left2right.
-            vport |= 3 << led_left_to_right_on;
-            physical_ports_to_virtual_port(vport);
-            if (led_left_to_right_on == 6){
+            vport |= 3 << led_on_index;
+            virtual_port_to_physical_port(vport);
+            led_on_index++;
+            if (led_on_index > 6){
 
                 // Both port's have had all LEDS moved left2right.  So
-                // setup  type name(args)  const;lags and  varibles to
+                // setup  type name(args)   and  varibles to
                 // the correct  state for  moving right to  left.  And
                 // for the next time LEDs need to go from left2right.
 
                 // Serial.println("dbg:Test to set Going Backwards");
 
                 // Time to go backwards, or right to left.
-                turn_on_leds_from_left2right = 0; 
-
-                led_right_to_left_on = 6;
-            
-                led_left_to_right_on = 0;
-
+                turn_on_leds_from_left2right = 0;
             }
-            led_left_to_right_on++;
+            
 
 
         } // end:: if (turn_on_leds_from_left2right)
         else if (!turn_on_leds_from_left2right){
             // Turn on LEDs right to left, or backwards
             Serial.print("dbg: right to left led_right_to_left_on=");
-            Serial.println(led_right_to_left_on);
+            Serial.println(led_on_index);
                 
 
             // Turn on the LEDs of portB from right to left
                 
             //Serial.println("dbg: Going Backwards");
-            vport |= (3 << led_right_to_left_on);
-            physical_ports_to_virtual_port(vport);
+            vport |= (3 << led_on_index);
+            virtual_port_to_physical_port(vport);
+            led_on_index--;
 
-            if (led_right_to_left_on == 0){
+            if (led_on_index < 0){
                 turn_on_leds_from_left2right = 1;
-                hold_next = 1;
+                led_on_index = 0;
+                //hold_next = 1;
             }
-            led_right_to_left_on--;
-
             
         }// end:: if (!turn_on_leds_from_left2right)
 
