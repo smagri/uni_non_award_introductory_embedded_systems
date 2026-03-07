74b7405e39c32c64ee74729e8913ad82726c79ac
Simone Magri <simone.j.magri@gmail.com>
2026-03-07 11:52:01 +1100

    lab4_playtime, Buzzer sounds one frequency for 10 seconds, time chosen for testing.

--- lab4/lab4_playtime/src/main.cpp
+++ lab4/lab4_playtime/src/main.cpp
@@ -4,7 +4,9 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define passive_buzzer PINB0 // passive buzzer pin, portB pin 0
+// passive buzzer pin, portB pin 0 PINB0, when you follow the header
+// files it is PB0 pin which just equates to 0.
+#define passive_buzzer PINB0
 #define active_buzzer PINB1 // active buzzer pin, portB pin 1
 
 
@@ -28,21 +30,20 @@ int main(void)
     float frequency[] = {523.25};
     float *ptr_frequency = &frequency[0];
 
-    // Duty cycle  is the ratio of  PWM  time_signal_high/period.
-    // The bigger the duty cycle the louder the buzzer.
-//    float duty_cycle = 0.1*1e6; // 1/10 of a cycle/period in us.
-    //float duty_cycle = 3.4028235e1; // 1/10 of a cycle/period in us.
-    float duty_cycle = 0.5; // 1/10 of a cycle/period in us.
+    // Duty  cycle is  the  ratio of  PWM time_signal_high/period.   The
+    // bigger the duty cycle the louder the buzzer.
+    //
+    // I like to think of the duty cycle as a percentage.  Thus 0.1 duty
+    // cycle means (0.1 *  100) %, or 10%.  Thus the  time the signal is
+    // high is 10% of the period.
+    float duty_cycle = 0.1; // 1/10 of the period of the sinal is high
 
     // There are 1e6us in a second. `That is 1*10^6us in a second. 
-    //int play_time = 30*1e6; // play sound in us 30s
-    //int play_time = 3.4028235e38; // play sound in us 30s
-    //int play_time = 1e6; // play sound in us 30s
-    //long play_time = 30*1e6
-    unsigned long play_time = 1e7;
+    // unsigned long play_time = 10*1e6; // play for 10 seconds
+    unsigned long play_time = 10*1e6; // play for 10 seconds
     
 
-    while (1) {
+    //   while (1) {
 
         // Play all frequencys in frequency array.
         for (unsigned char i=0; i<sizeof(frequency)/sizeof(float); i++) {
@@ -50,7 +51,7 @@ int main(void)
             playSound(*(ptr_frequency+i), duty_cycle, play_time);            
         }
         
-    }
+        // }
     
 
 
@@ -66,25 +67,26 @@ int main(void)
 void playSound(float frequency, float duty_cycle, unsigned long play_time){
 
     float period_us = (1/frequency)*1e6; // Period in microseconds.
-    float time_signal_high_us = (duty_cycle * period_us); // in us
-    float time_signal_low_us = (period_us - time_signal_high_us); // in us
-    unsigned long cycles;
+//    float time_signal_high_us = (duty_cycle * period_us); // in us
+    unsigned int time_signal_high_us = (duty_cycle * period_us); // in us
+    unsigned int time_signal_low_us = (period_us - time_signal_high_us); //in us
+    // Number of times our for loop needs to excute.
+    unsigned long cycles_for_playtime; 
     
-//    for (int j=0; j<(play_time/period_us); j++){
-    cycles = ( (unsigned long)(play_time/period_us) );
-    for (unsigned long j=0; j<cycles; j++){
-        //    for (int j=0; j<play_time; j++){
+
+    // cycles  is how  many  periods  fit into  the  whole playing  time
+    // required.  As  one  invocation  of  our for  loop  below  is  one
+    // period. in length.
+    cycles_for_playtime = ( (unsigned long)(play_time/period_us) );
+
+    for (unsigned long j=0; j<cycles_for_playtime; j++){
 
         // Play for one Period forech high-low combinations
-        PORTB = PORTB | (1<<passive_buzzer); // Set output of portB pin0 to high
-        for (int i=0; i<time_signal_high_us; i++) {
-            _delay_us(1);
-        }
-        PORTB = PORTB & ~(1<<passive_buzzer); // Set output of portB pin0 to low
-        for (int i=0; i<time_signal_low_us; i++){
-            _delay_us(1);
-        }
+        PORTB |= (1<<passive_buzzer); // Set output of portB pin0 to high
+        _delay_us(time_signal_high_us);
 
+        PORTB &= ~(1<<passive_buzzer); // Set output of portB pin0 to low
+            _delay_us(time_signal_low_us);
     }
     
 }
